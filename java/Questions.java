6a38a544deec02157b2cc3b748c4659a9bb9d97a
nikkamarino8 <111229786+nikkamarino8@users.noreply.github.com>
2022-11-28 21:27:10 +0800

    update

---
+++ java/Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    String quest;
+    String answer;
+
+    public Questions(String quest, String answer){
+        this.quest = quest;
+        this.answer = answer;
+
+    }
+
+}
